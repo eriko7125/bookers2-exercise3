7904c66273c7343f3c1051bac09535be74af20d1
eriko7125 <t.eriko7125@gmail.com>
2020-02-11 07:46:49 +0000

    Fix: book_comment destroy

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,:validatable
+
+  has_many :books
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  attachment :profile_image, destroy: false
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: {maximum: 50}
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users #これが先
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  root 'home#top'
+  get 'home/about'
+end
